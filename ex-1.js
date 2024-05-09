fc1d8f8a1ea448a54330a389e27c3930a9bcbbe4
revieww12 <159617994+revieww12@users.noreply.github.com>
2024-05-09 14:53:39 +0700

    Update ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -2,5 +2,5 @@ let isOver18 = false;
 let hasCriminalBlacklist = false;
 
 let isAllow;
-isAllow = isOver18 && hasCriminalBlacklist;
+isAllow = isOver18 && !hasCriminalBlacklist;
 console.log(isAllow);
